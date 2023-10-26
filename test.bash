e3910e457e4733cf5ade733ae7c016d719b50397
Murakoshi Ayaka <Akaya@ume>
2023-10-26 13:24:38 +0900

    hirou

--- test.bash
+++ test.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
-
+ng () {
+         echo NG at Line $1 #$1はngの1行目の引数　最後に返す終了の文字をセット
+         let=1
+	}
+let=0
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ]
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$let" = 0 ] && echo OK
+exit $let
